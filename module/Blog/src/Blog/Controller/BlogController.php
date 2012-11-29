1dee25a76d8c98ff71def49b32fb0761f09df697
Dmitri Ischenko <ischenkodv@gmail.com>
2012-11-30 00:36:03 +0200

    Added the Blog module

---
+++ module/Blog/src/Blog/Controller/BlogController.php
@@ -0,0 +1,24 @@
+<?php
+namespace Blog\Controller;
+
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewMode;
+
+class BlogController extends AbstractActionController
+{
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
