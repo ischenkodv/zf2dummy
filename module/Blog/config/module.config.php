b539fd31c61ca0f20c7a91f1d92c803347a25ce6
Dmitri Ischenko <ischenkodv@gmail.com>
2012-12-02 18:39:48 +0200

    Fixed bug with routes badly configured for the Blog module.

--- module/Blog/config/module.config.php
+++ module/Blog/config/module.config.php
@@ -4,20 +4,20 @@ return array(
         'invokables' => array(
             'Blog\Controller\Blog' => 'Blog\Controller\BlogController'
         ),
-        'router' => array(
-            'routes' => array(
-                'blog' => array(
-                    'type'    => 'segment',
-                    'options' => array(
-                        'route'    => '/blog[/:action][/:id]',
-                        'constraints' => array(
-                            'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
-                            'id'     => '[0-9]+',
-                        ),
-                        'defaults' => array(
-                            'controller' => 'Blog\Controller\Blog',
-                            'action'     => 'index',
-                        ),
+    ),
+    'router' => array(
+        'routes' => array(
+            'blog' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/blog[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Blog\Controller\Blog',
+                        'action'     => 'index',
                     ),
                 ),
             ),
